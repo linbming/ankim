eba717bb607ad38ceb8a9ac75bdc374f6abbf359
linbming <58204617+linbming@users.noreply.github.com>
2019-11-26 15:03:09 +0800

    Update text.c

--- text.c
+++ text.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-void main()
-{
-printf("hello,world!!!");
-}
+language: c 
+dist: bionic 
+sudo: required 
+install: 
+  - sudo apt update 
+  - sudo apt-get install -y libcurl4-openssl-dev 
+script:  
+  - gcc hello.c -o hello -lcurl 
+  - ./hello 
